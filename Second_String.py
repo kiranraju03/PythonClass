55c2dfc972a7843b0a100df02d6df0b6e973b229
kiranraju03 <kiran.raju.k.43@gmail.com>
2017-02-07 20:31:28 +0530

    String functionality in python

---
+++ Second_String.py
@@ -0,0 +1,11 @@
+name="My name is Kiran Raju"
+FirstName=name[11:16]
+print(FirstName)
+LastName=name[-4:]
+print(LastName)
+Rest=name[:10]
+print(Rest)
+a="Kiran"
+b="Raju"
+c="Krishna"
+print(a+' '+b+' '+c)
