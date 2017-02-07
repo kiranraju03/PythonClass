6530ef75eaa43bcb659737d289b012f4e5a31fe0
kiranraju03 <kiran.raju.k.43@gmail.com>
2017-02-07 21:04:04 +0530

    Different datatype structures for storing information in python with dictionary functionalities

--- Third_DS.py
+++ Third_DS.py
@@ -22,3 +22,7 @@ print(dicy.values())
 print(type(dicy.values()))
 lisy=list(dicy.values())
 print(lisy)
+
+money={"saving_account":200, "checking_account":100, "under_bed":[500,10,100]}
+k=money["under_bed"][1]
+print(k)
